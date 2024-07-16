5c927b9af5b84943bb366f0201ffa34fc80ff92f
rajnikhar29 <rajnikhar2903@gmail.com>
2024-07-16 12:31:03 +0530

    Time: 603 ms (9.54%), Space: 54.2 MB (6.63%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int maxJump=0;
+    bool f(int ind,int n,vector<int>&nums,vector<int>&dp){
+        if(ind==n-1) return true;
+        // if(ind>=n) return true;
         
-        for(int i=0;i<nums.size();i++){
-            if(i > maxJump) return false;//max agr 3 h aur hum 4th index pr aachuke so false
+        if(dp[ind]!=-1) return dp[ind];
+        for(int i=1;i<=nums[ind];i++){
+            if(f(ind+i,n,nums,dp)==true) return true;
             
-            maxJump=max(maxJump,i+nums[i]);
         }
-        return true;
+        return dp[ind]=false;
+        
+    }
+    
+    bool canJump(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        return f(0,n,nums,dp);
+        
+        
+//         int maxJump=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(i > maxJump) return false;//max agr 3 h aur hum 4th index pr aachuke so false
+            
+//             maxJump=max(maxJump,i+nums[i]);
+//         }
+//         return true;
     }
 };
