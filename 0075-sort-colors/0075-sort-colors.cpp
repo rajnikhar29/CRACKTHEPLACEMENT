3267e45fd65a31c4c781a4ea5b91bfb4b1e42857
rajnikhar29 <rajnikhar2903@gmail.com>
2024-07-17 13:39:51 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (5.84%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                ans.push_back(0);
+      int n=nums.size();
+        int i=0;
+        int j=0;
+        int k=n-1;
+        
+        while(j<=k){
+            if(nums[j]==1) j++;
+            else if(nums[j]==2){
+                swap(nums[j],nums[k]);
+                k--;
             }
+            else{
+                swap(nums[j],nums[i]);
+                i++;
+                j++;
             }
-        for(int i=0;i<n;i++){
-            if(nums[i]==1){
-                ans.push_back(1);
-            }
-            
         }
-        for(int i=0;i<n;i++){
-            if(nums[i]==2){
-                ans.push_back(2);
-            }
-
-        }
-        nums=ans;
+        
+        
+        
+        
+        // int n=nums.size();
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==0){
+        //         ans.push_back(0);
+        //     }
+        //     }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==1){
+        //         ans.push_back(1);
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==2){
+        //         ans.push_back(2);
+        //     }
+        // }
+        // nums=ans;
 
         
     }
