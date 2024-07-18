9f91848e31cdeb74431b6e82e7243c2d7bc0231b
rajnikhar29 <rajnikhar2903@gmail.com>
2024-07-18 13:20:34 +0530

    Time: 6 ms (26.95%), Space: 12.4 MB (76.71%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
-vector<string>result;
+    vector<string> result;
 
-bool isValid(string&str){
-    int count=0;
-    for(char ch :str){
-        if(ch=='(') count++;
-        else count--;
-        if(count <0) return false;
+//     bool isValid(string& str) {
+//         int count = 0;
+//         for (char ch : str) {
+//             if (ch == '(')
+//                 count++;
+//             else
+//                 count--;
+//             if (count < 0)
+//                 return false;
+//         }
+//         return count == 0;
+//     }
 
-    }
-    return count==0;
-}
-
-void solve(string&curr,int n){
-    if(curr.length()==2*n){
-        if(isValid(curr)){
+    void solve(string& curr, int n,int open,int close) {
+        if (curr.length() == 2 * n) {
             result.push_back(curr);
+            return;
         }
-        return;
+        if(open<n){
+        curr.push_back('(');
+        solve(curr, n,open+1,close);
+        curr.pop_back();
+        }
+        if(close<open){
+        curr.push_back(')');
+        solve(curr, n,open,close+1);
+        curr.pop_back();
+        }
     }
-
-    curr.push_back('(');
-    solve(curr,n);
-    curr.pop_back();
-
-    curr.push_back(')');
-    solve(curr,n);
-    curr.pop_back();
-}
     vector<string> generateParenthesis(int n) {
-        string curr="";
+        string curr = "";
+        int open=0;
+        int close=0;
 
-        solve(curr,n);
+        solve(curr, n,open,close);
         return result;
-
     }
 };
