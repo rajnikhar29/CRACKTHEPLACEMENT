7fe5b9bc1a66d1b3fbf78574488acd4d9c0505ec
rajnikhar29 <rajnikhar2903@gmail.com>
2024-08-01 13:48:49 +0530

    Time: 11 ms (12.78%), Space: 17 MB (6.61%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -6,18 +6,64 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode*prev=NULL;
-    void flatten(TreeNode* root) {
-        if(root==NULL) return;
-        flatten(root->right);
-        flatten(root->left);
-        root->right=prev;
-        root->left=NULL;
-        prev=root;
+    // T.C-O(N),S.C-O(N)--TODO JODO
+    // TreeNode*prev=NULL;
+    // void flatten(TreeNode* root) {
+    //     if(root==NULL) return;
+    //     flatten(root->right);
+    //     flatten(root->left);
+    //     root->right=prev;//connecting link
+    //     root->left=NULL;
+    //     prev=root;
+    // }
+
+    // T.C-O(N),S.C-O(N)
+    // void flatten(TreeNode* root) {
+    //     if(root==NULL) return;
+    //     stack<TreeNode*> st;
+    //     st.push(root);
+
+    //     while (!st.empty()) {
+    //         TreeNode* curr = st.top();
+    //         st.pop();
+
+    //         if (curr->right)
+    //             st.push(curr->right);
+    //         if (curr->left)
+    //             st.push(curr->left);
+
+    //         if (!st.empty())
+    //             curr->right = st.top(), curr->left = NULL;
+    //     }
+    // }
+
+    void flatten(TreeNode* root){
+        TreeNode*curr=root;
+
+        while(curr!=NULL){
+            if(curr->left!=NULL){
+            TreeNode*prev=curr->left;
+
+                while(prev->right!=NULL){
+                    prev=prev->right;
+                }
+                
+                prev->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+                
+            }
+            curr=curr->right;
+                
+
+        }
     }
+
+    
 };
