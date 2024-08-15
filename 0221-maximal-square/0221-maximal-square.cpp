b6ad8f640499f649e3f25896fa1181031e05e06d
rajnikhar29 <rajnikhar2903@gmail.com>
2024-08-15 15:41:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(int i , int j ,vector<vector<char>>& matrix,int n, int m,int& maxi, vector<vector<int>>&dp){
+        if(i>=n || j>=m ) return 0;
+
+        int right     = solve(i,j+1,matrix, n, m,maxi,dp);
+        int down      = solve(i+1,j,matrix,n,m,maxi,dp);
+        int diagnoal  = solve(i+1,j+1,matrix,n,m,maxi,dp);
+        int curr =0;
+        if(matrix[i][j]=='1'){
+             curr = 1 + min(right , min(down, diagnoal));//legit square h ya nhi
+             maxi = max(maxi ,curr);
+            return dp[i][j] =curr;
+        }else{
+
+            return dp[i][j]=0;
+        }
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int maxi = 0;
+         solve(0,0,matrix,n,m,maxi,dp);
+
+         return maxi * maxi;
+    }
+};
