2147360f4e46468a1bca02c6ee1acc48a1a737c9
rajnikhar29 <rajnikhar2903@gmail.com>
2024-08-30 14:25:42 +0530

    Time: 29 ms (42.45%), Space: 23.9 MB (13.71%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,27 +10,50 @@
  */
 class Solution {
 public:
+ListNode*curr;
+void solve(ListNode*head){
+    if(head==NULL) return ;
+    solve(head->next);
+
+    ListNode*temp=curr->next;
+    if(curr->next==NULL) return;
+    else if(head==curr) {
+        head->next=NULL;
+        return;
+    }
+
+    curr->next=head;
+    head->next=(temp==head)?NULL:temp;
+    curr=temp;
+
+}
     void reorderList(ListNode* head) {
-        stack<ListNode*>st;
-        ListNode*curr=head;
-        while(curr){
-            st.push(curr);
-            curr=curr->next;
-        }
 
-        int n=st.size()/2;
-        curr=head;
 
-        while(n--){
-            ListNode*topNode=st.top();
-            st.pop();
+       curr=head;
+       solve(head);
 
-            ListNode*temp=curr->next;
-            curr->next=topNode;
-            topNode->next=temp;
-            curr=temp;
-        }
-        curr->next=NULL;
+        // // O(N) ,S.C-O(N);
+        // stack<ListNode*>st;
+        // ListNode*curr=head;
+        // while(curr){
+        //     st.push(curr);
+        //     curr=curr->next;
+        // }
+
+        // int n=st.size()/2;
+        // curr=head;
+
+        // while(n--){
+        //     ListNode*topNode=st.top();
+        //     st.pop();
+
+        //     ListNode*temp=curr->next;
+        //     curr->next=topNode;
+        //     topNode->next=temp;
+        //     curr=temp;
+        // }
+        // curr->next=NULL;
 
 
     }
